5ae99d9180e2819f3eaf3e7d245bfcdb5deb11c1
Antoine Libert <contact@antoine-libert.com>
2025-12-03 09:47:44 +0100

    fix: titlebar exit

--- src/stores/titleBarOptionsStore.ts
+++ src/stores/titleBarOptionsStore.ts
@@ -1,8 +1,17 @@
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import { defineStore } from 'pinia'
+import { useRouter } from 'vue-router'
 
 export const useTitleBarOptionsStore = defineStore('titleBarOptions', () => {
+  const router = useRouter()
   const search = ref('')
   const isThemingBarOpen = ref(false)
+
+  watch(router.currentRoute, newRoute => {
+    if (newRoute.name !== 'home') {
+      isThemingBarOpen.value = false
+    }
+  })
+
   return { search, isThemingBarOpen }
 })
